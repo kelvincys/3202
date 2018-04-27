d5ca16db6887e2c0310ba257d116bddb8b5752cf
Sam Lai <l.lai4412579@gmail.com>
2018-04-27 15:55:18 +1000

    .

--- wedding.php
+++ wedding.php
@@ -15,7 +15,7 @@
 
     <!-- Custom styles for this template -->
     <link href="css/basic.css" rel="stylesheet">
-    <link href="css/contactus.css" rel="stylesheet">
+    <link href="css/wedding.css" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -80,12 +80,13 @@
                 <span class="line -bottom"></span>
               </a>
             </li>
-
           </ul>
         </div>
       </div>
     </nav>
-
+    <center>
+    <img src="assert/008.jpg" width=100%></img>
+    </center>
 
   
   </body>
