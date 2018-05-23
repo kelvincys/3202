09bbd1ede9184b5a5789ffc3814741b28fb69c3c
Kelvincys <kelvinchungkw@hotmail.com>
2018-05-23 10:41:35 +0800

    contact

--- contacting.php
+++ contacting.php
@@ -17,7 +17,7 @@
         $result = execute_sql($link, "moment", $sql) or die(mysqli_error($link));
 	}
 	$subject = "Thank you for contact us";
-	$message = "Dear".$name. "\n Thank you for contacting us. \n We will reply you soon";
+	$message = "Dear ".$name. "\n Thank you for contacting us. \n We will reply you soon \n If you did not recieve an email from us in the next week, please send an email to enquiry@moment.com \n\n Moment 2018\nTel:02-1234-1234\nEmail:enquiry@moment.com";
 	$headers = "From: kelvinchungkw@gmail.com";
 	mail($to,$subject,$message,$headers);
 	header('Location: ' . $_SERVER['HTTP_REFERER']);
