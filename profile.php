9ebd8e8162cd0c1b1b5e159e46affd803503d8d5
Kelvincys <kelvinchungkw@hotmail.com>
2018-05-24 09:32:41 +0800

    .

--- profile.php
+++ profile.php
@@ -25,6 +25,7 @@ if(!isset($_SESSION['logined'])){
     <link href="https://fonts.googleapis.com/css?family=Do+Hyeon|IM+Fell+DW+Pica" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/basic.js"></script>
 
   </head>
 
